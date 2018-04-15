abb93dafae6288a1be757da5b53a86aff83b48b3
Carlosx71 <carlos.x71@hotmail.com>
2018-04-15 19:14:32 -0300

    Criacao do metodo "validarLogin".

--- SIASS/Events/Validate.js
+++ SIASS/Events/Validate.js
@@ -8,3 +8,18 @@ function mostrarSenha() {
 		senha.type = "password";
 	}
 }
+
+function validarLogin() {
+
+	var usuario = document.getElementById('usuario').value;
+	var senha = document.getElementById('senha').value;
+
+	if (usuario == "admin" && senha == "admin") {
+		//trocar caminho para a index
+		window.location = "../Forms/Cadastro_Artesao.html";
+		done = 1;
+	} if (done==0) {
+		
+		alert("Usuario ou senha invalidos");
+	}
+}
